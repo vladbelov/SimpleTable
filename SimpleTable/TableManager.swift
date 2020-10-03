031613214178f9950ecb7c0833dd4da6bd1a79ff
Vladislav Belov <com.belov@gmail.com>
2020-10-03 17:59:49 +0300

    WIP

---
+++ SimpleTable/TableManager.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleTable.swift
+//  SimpleTable
+//
+//  Created by Белов В.С. on 03.10.2020.
+//  Copyright © 2020 Белов В.С. All rights reserved.
+//
+
+import UIKit
+
+public final class SimpleTable: NSObject {
+    
+    private let tableView: UITableView
+    
+    init(tableView: UITableView) {
+        self.tableView = tableView
+    }
+    
+}
+
+extension SimpleTable: UITableViewDataSource {
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
+    
+}
+
+extension SimpleTable: UITableViewDelegate {
+    
+}
