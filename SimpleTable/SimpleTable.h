b684da880edaeda57c539e9d032b36cee6cac48f
vs.belov <vs.belov@tensor.ru>
2020-10-03 10:29:18 +0300

    Initial Commit

---
+++ SimpleTable/SimpleTable.h
@@ -0,0 +1,19 @@
+//
+//  SimpleTable.h
+//  SimpleTable
+//
+//  Created by Белов В.С. on 03.10.2020.
+//  Copyright © 2020 Белов В.С. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleTable.
+FOUNDATION_EXPORT double SimpleTableVersionNumber;
+
+//! Project version string for SimpleTable.
+FOUNDATION_EXPORT const unsigned char SimpleTableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleTable/PublicHeader.h>
+
+
