605537251b9b4e998112683c2185ab9eecd94468
vs.belov <vs.belov@tensor.ru>
2020-10-03 10:51:05 +0300

    Travis init

---
+++ SimpleTable/SimpleTable.swift
@@ -0,0 +1,11 @@
+//
+//  SimpleTable.swift
+//  SimpleTable
+//
+//  Created by Белов В.С. on 03.10.2020.
+//  Copyright © 2020 Белов В.С. All rights reserved.
+//
+
+public final class SimpleTable {
+    
+}
