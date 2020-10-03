9e7051d02772939ec44c245015e8a9bc598772a0
vs.belov <vs.belov@tensor.ru>
2020-10-03 10:36:03 +0300

    Init

---
+++ SimpleTableExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleTableExamples
+//
+//  Created by Белов В.С. on 03.10.2020.
+//  Copyright © 2020 Белов В.С. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
